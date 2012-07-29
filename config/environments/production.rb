5f5b2eca864f21ab8e45a9401e6b01dd2494c21c
Alexander Negoda <alexander.negoda@gmail.com>
2012-07-30 03:10:12 +0400

    fix default_url_options for production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,10 +46,9 @@ Photomix::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( front.js front_preloader.js )
-
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.assets.precompile += %w( ajaxful_rating.css )
 
   # Enable threaded mode
   # config.threadsafe!
@@ -60,6 +59,7 @@ Photomix::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  set default_url_options[:host] = 'example.com'
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
